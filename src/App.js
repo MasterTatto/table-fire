7dda842907bd698b4d60739c01e3c3805f084245
MasterTatto <gromvolk97@gmail.com>
2024-09-19 02:13:07 +0300

    init

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
                 <Input placeholder={'ПОИСК по никнеймам?'}/>
                 <Input placeholder={'Фильтр по датам, типам контрактов?'}/>
             </div>
+
             <h2 className={'title'}>Страница контрактов</h2>
             <GridExample/>
         </div>
